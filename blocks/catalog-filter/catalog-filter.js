3618fe37f12f1fd5b9b08ede12468a9c35c2c69c
A.Zhukouski <a.zhukouski@softarex.com>
2021-02-08 12:45:35 +0300

    added fixed catalog filter on scroll, corrected layout r-side border(for catalog filter)

--- blocks/catalog-filter/catalog-filter.js
+++ blocks/catalog-filter/catalog-filter.js
@@ -14,4 +14,34 @@ document.addEventListener('DOMContentLoaded', function () {
 			});
 		})
 	})
+
+
+	let filterHeight = $('.catalog-filter').height(),
+		filterTop = $('.layout__row.header').outerHeight(),
+		filterLeft = $('.layout__row:not(.header) .layout__l-side').outerWidth(),
+		filterRight = $('.layout__row:not(.header) .layout__r-side').outerWidth();
+	console.log(filterLeft)
+
+	$('.catalog__filter-wrap').height(filterHeight);
+
+	let itemTop = $('.catalog-filter').offset().top - filterTop;
+	$(window).scroll(function (event) {
+		// определяем позицию y относительно окна браузера
+		let windowTop = $(this).scrollTop();
+
+		// если он является формой ниже
+		if (windowTop >= itemTop) {
+			// то присваиваем класс .fixed
+			$('.catalog-filter').addClass('fixed');
+			$('.catalog-filter').css({
+				'top':filterTop,
+				'left':filterLeft,
+				'right':filterRight
+			});
+		} else {
+			// если нет – удаляем класс
+			$('.catalog-filter').removeClass('fixed');
+			$('.catalog-filter').attr('style', '');
+		}
+	});
 });
