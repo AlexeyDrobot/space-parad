0774e744e788e2031ebee87120a11918e1c9a643
A.Zhukouski <a.zhukouski@softarex.com>
2021-02-13 20:57:01 +0300

    codding catalog.html + filter script

--- blocks/catalog/catalog.js
+++ blocks/catalog/catalog.js
@@ -10,10 +10,22 @@ document.addEventListener('DOMContentLoaded', function () {
 		arrows: false
 	});
 
-	$('.js-xxx').slick({
-		dots: true,
-		infinite: false,
+	$('.catalog__popular-slider').slick({
+		//centerMode: true,
+		//centerPadding: '0',
+		dots: false,
+		arrows: true,
+		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
+	});
+	$('.catalog__article-slider').slick({
+		dots: true,
+		infinite: true,
+		fade: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true
 	});
 });
