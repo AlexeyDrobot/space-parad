4c3c8edab54c2cdcc566c6ac5921785aeace062c
A.Zhukouski <a.zhukouski@softarex.com>
2021-02-26 19:34:25 +0300

    fixed the scroll block(product.html)

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,55 +1,6 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  $('.catalog__product-slider').slick({
-    dots: true,
-    infinite: true,
-    fade: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false
-  });
-  $('.catalog__popular-slider').slick({
-    dots: false,
-    arrows: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        centerMode: true,
-        centerPadding: '100px',
-        slidesToShow: 1
-      }
-    }, {
-      breakpoint: 500,
-      settings: {
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 1
-      }
-    }, {
-      breakpoint: 375,
-      settings: {
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 1
-      }
-    }]
-  });
-  $('.catalog__article-slider').slick({
-    dots: true,
-    infinite: true,
-    fade: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true
-  });
-});
-document.addEventListener('DOMContentLoaded', function () {
   $('.js-filter-item').each(function (index, value) {
     var attrValue = $(this).attr('aria-label');
     $(this).niceSelect().next().attr('aria-label', attrValue);
@@ -141,6 +92,55 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     $('body').removeClass('no-scroll');
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  $('.catalog__product-slider').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+  $('.catalog__popular-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 500,
+      settings: {
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1
+      }
+    }, {
+      breakpoint: 375,
+      settings: {
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }]
+  });
+  $('.catalog__article-slider').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true
   });
 }); // Скрипт добавления файлов в форму
 
